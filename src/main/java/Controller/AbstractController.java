e03be2bded1ca3c8fb965e0b49b06c03cce71f62
Frank Orozco <frank_miguel29@outlook.com>
2019-11-19 06:28:28 -0900

    Abstract Controller class updated

--- src/main/java/Controller/AbstractController.java
+++ src/main/java/Controller/AbstractController.java
@@ -1,17 +1,21 @@
 package Controller;
 
-
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Cursor;
 import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-
-    class AbstractController {
+import javafx.util.Duration;
+import java.io.IOException;
+class AbstractController {
 
     private static double x, y;
-    Stage stage;
-
 
     static void closeApp(ImageView close){
 
@@ -39,6 +43,35 @@ import javafx.stage.Stage;
         });
 
         pane.setOnMouseReleased(event -> pane.setCursor(Cursor.DEFAULT));
+
+    }
+
+    void switchStage(Node altControl, String Layout){
+
+        altControl.setOnMouseClicked(event -> {
+
+            try {
+
+                Parent root = FXMLLoader.load(getClass().getResource("../Layout/"+(Layout)+".fxml"));
+                root.translateXProperty().set(altControl.getScene().getWidth());
+                Pane pane = (Pane) altControl.getScene().getRoot();
+                pane.getChildren().removeAll(); //Improvement of Performance: Deleting all previous nodes before set the next Parent
+                pane.getChildren().add(root);
+
+                Timeline timeline = new Timeline();
+                KeyValue kv = new KeyValue(root.translateXProperty(), 0, Interpolator.DISCRETE);
+                KeyFrame kf = new KeyFrame(Duration.millis(100), kv);
+                timeline.getKeyFrames().add(kf);
+                timeline.play();
+
+            } catch (IOException e) {
+
+                System.out.println("Error switching stages");
+
+            }
+
+
+        });
 
     }
 
